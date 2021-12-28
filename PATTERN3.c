8660f8509143a9252cedc7433cbf3f0da4f77ff3
attuz <96783881+attuz@users.noreply.github.com>
2021-12-28 22:31:30 +0530

    Add files via upload

---
+++ PATTERN3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int n,m,i,j;
+	printf("Enter no. of rows:");
+	scanf("%d",&n);
+	for(i=n;i>0;i--)
+	{
+		for(j=i;j>0;j--)
+		{
+			printf("*\t");
+		}
+		printf("\n");
+	}
+}

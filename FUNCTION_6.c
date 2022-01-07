707616b938280fb1430329b3ae2e95c9d9b54bd8
Aneetta Paul <96783881+attuz@users.noreply.github.com>
2022-01-07 18:35:24 +0530

    Add files via upload

---
+++ FUNCTION_6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap();
+void main()
+{
+	swap();
+}
+void swap()
+{
+	int a,b,c;
+	printf("Enter two numbers to swap:");
+	scanf("%d%d",&a,&b);
+	c=a;
+	a=b;
+	b=c;
+	printf("After swapping:%d %d",a,b);
+}
+	
